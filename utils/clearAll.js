958c302d819976547539d741ee40a043f55e9dae
MNoumanAli <mnoumanali001@gmail.com>
2023-08-30 00:09:58 +0500

    Utility function to clear inputs

---
+++ utils/clearAll.js
@@ -0,0 +1,5 @@
+export const clearAll = (field1, field2, field3) => {
+  field1.innerText = "";
+  field2.innerText = "";
+  field3.value = "";
+};
---
+++ utils/clearInput.js
@@ -0,0 +1,4 @@
+export const clearInput = (field) => {
+  field.innerText = field.innerText?.slice(0, -1);
+  //editVal.value = inputVal.innerText
+};
